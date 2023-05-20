17d022e4a5edeb6a4fd48b6d31292b425181d1c6
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-05-20 13:18:39 +0900

    fix api method

--- star.go
+++ star.go
@@ -2,15 +2,20 @@ package main
 
 import "regexp"
 
-func getStarPageURL(headerLink string) (string, string) {
-	var nextPage, lastPage string
+func getNextPageURL(headerLink string) string {
+	var nextPage string
 	nextRe := regexp.MustCompile("<([^>]+)>; rel=\"next\"")
 	nextMatch := nextRe.FindStringSubmatch(headerLink)
 	nextPage = nextMatch[1]
 
+	return nextPage
+}
+
+func getLastPageURL(headerLink string) string {
+	var lastPage string
 	lastRe := regexp.MustCompile("<([^>]+)>; rel=\"last\"")
 	lastMatch := lastRe.FindStringSubmatch(headerLink)
 	lastPage = lastMatch[1]
 
-	return nextPage, lastPage
+	return lastPage
 }
