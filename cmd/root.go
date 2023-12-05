f243f23882324a54aa2d3bf7ed33768ac6e83aa2
Ryotaro <70475997+RyotaroSeto@users.noreply.github.com>
2023-12-06 08:19:52 +0900

    Update root.go

--- cmd/root.go
+++ cmd/root.go
@@ -3,55 +3,43 @@ package cmd
 import (
 	"context"
 	"log"
-	"os"
 	"os/signal"
-	"star-golang-orms/configs"
-	"star-golang-orms/pkg"
+	"star-golang-orms/infra"
 	"syscall"
 )
 
 func Execute() {
-	ctx, cancel := NewCtx()
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer cancel()
 
-	config, err := configs.LoadConfig(".")
+	err := infra.Load(".")
 	if err != nil {
 		log.Fatal("cannot load config", err)
 		return
 	}
 
-	gh, err := pkg.ExecGitHubAPI(ctx, config.GithubToken)
-	if err != nil {
-		log.Fatal("cannot exec github api", err)
-		return
-	}
+	log.Println(ctx)
+	// gh, err := pkg.ExecGitHubAPI(ctx, config.GitHubToken)
+	// if err != nil {
+	// 	log.Fatal("cannot exec github api", err)
+	// 	return
+	// }
 
-	err = gh.SortDesByStarCount()
-	if err != nil {
-		log.Fatal("cannot sort star count", err)
-		return
-	}
+	// err = gh.SortDesByStarCount()
+	// if err != nil {
+	// 	log.Fatal("cannot sort star count", err)
+	// 	return
+	// }
 
-	err = gh.MakeChart()
-	if err != nil {
-		log.Fatal("cannot make chart", err)
-		return
-	}
+	// err = gh.MakeChart()
+	// if err != nil {
+	// 	log.Fatal("cannot make chart", err)
+	// 	return
+	// }
 
-	err = gh.Edit()
-	if err != nil {
-		log.Fatal("cannot edit readme", err)
-		return
-	}
-}
-
-func NewCtx() (context.Context, context.CancelFunc) {
-	ctx, cancel := context.WithCancel(context.Background())
-	go func() {
-		trap := make(chan os.Signal, 1)
-		signal.Notify(trap, syscall.SIGTERM, syscall.SIGHUP, syscall.SIGINT)
-		<-trap
-	}()
-
-	return ctx, cancel
+	// err = gh.Edit()
+	// if err != nil {
+	// 	log.Fatal("cannot edit readme", err)
+	// 	return
+	// }
 }
