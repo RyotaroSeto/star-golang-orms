9330ecd18369d447705130d7a713ff8f4e0b4650
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-12-08 00:01:00 +0900

    Update GetStar method to GetStarPage

--- domain/repository/github.go
+++ domain/repository/github.go
@@ -8,5 +8,5 @@ import (
 //go:generate go run go.uber.org/mock/mockgen -source=github.go -package=repository -destination=github_mock.go
 type GitHub interface {
 	GetRepository(ctx context.Context, rn model.RepositoryName) (*model.GitHubRepository, error)
-	GetStar(ctx context.Context) (int, error)
+	GetStarPage(ctx context.Context, repo model.GitHubRepository, page int) (*model.Stargazer, error)
 }
