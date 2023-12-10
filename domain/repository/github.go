59fdef611a44bcdd064656189f67c82ef06c853f
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-12-10 19:01:04 +0900

    Update GitHub interface methods

--- domain/repository/github.go
+++ domain/repository/github.go
@@ -7,6 +7,6 @@ import (
 
 //go:generate go run go.uber.org/mock/mockgen -source=github.go -package=repository -destination=github_mock.go
 type GitHub interface {
-	GetRepository(ctx context.Context, rn model.RepositoryName) (*model.GitHubRepository, error)
-	GetStarPage(ctx context.Context, repo model.GitHubRepository, page int) (*model.Stargazer, error)
+	GetRepository(ctx context.Context, rn model.RepositoryName) (*model.Repository, error)
+	GetStarPage(ctx context.Context, repo model.Repository, page int) (*[]model.Stargazer, error)
 }
