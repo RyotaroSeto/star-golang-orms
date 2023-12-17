1c22aa78d9a5a59c6685b0faeca94b98a9beb3d6
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-12-17 12:44:32 +0900

    Remove commented code and unused import in config.go

--- infra/config.go
+++ infra/config.go
@@ -8,28 +8,12 @@ import (
 )
 
 type Config struct {
-	// GitHubToken string `mapstructure:"GITHUB_TOKEN"`
 	GitHubToken string `env:"GITHUB_TOKEN,required"`
 }
 
 var c *Config
 
 func Load(ctx context.Context) error {
-	// viper.AddConfigPath(path)
-	// viper.SetConfigName("app")
-	// viper.SetConfigType("env")
-
-	// viper.AutomaticEnv()
-	// err := viper.ReadInConfig()
-	// if err != nil {
-	// 	return errors.New("cannot read config")
-	// }
-
-	// var cfg Config
-	// err = viper.Unmarshal(&cfg)
-	// if err != nil {
-	// 	return fmt.Errorf("cannot unmarshal config: %w", err)
-	// }
 	var cfg Config
 	if err := envconfig.Process(ctx, &cfg); err != nil {
 		return fmt.Errorf("cannot unmarshal config: %w", err)
